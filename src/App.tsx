0caef078c90e6508a97002cffc3f4364a9a87077
enoshima7 <duanjipeng0328@gmail.com>
2020-04-26 00:01:36 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Button, ButtonSize, ButtonType } from './components/Button/Button';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Button disabled>click</Button>
+      <Button btnType={ButtonType.Primary}>click</Button>
+      <Button btnType={ButtonType.Default}>click</Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
+        click
+      </Button>
+      <Button btnType={ButtonType.Link} href='#' size={ButtonSize.Small}>
+        click
+      </Button>
+      <Button btnType={ButtonType.Link} disabled href='#'>
+        click
+      </Button>
     </div>
   );
 }
